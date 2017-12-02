28b42b00ad9191976a6f41a89f9dc30470de160a
Tim Horist <TimelikeClosure@users.noreply.github.com>
2017-12-01 21:26:33 -0800

    added hello_world app skeleton

---
+++ hello_world/index.js
@@ -0,0 +1,14 @@
+"use strict";
+
+function runScript(){
+
+}
+
+function runNative(){
+
+}
+
+module.exports = module = {
+    script: runScript,
+    native: runNative
+};
---
+++ index.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const addonDir = "hello_world";
+const path = require('path');
+const addonPath = path.join(__dirname, addonDir, 'index');
+
+const { script, native } = require(addonPath);
+
+console.log("====RUNNING SCRIPT VERSION====");
+console.log(script);
+console.log("====RUNNING NATIVE VERSION====");
+console.log(native);
